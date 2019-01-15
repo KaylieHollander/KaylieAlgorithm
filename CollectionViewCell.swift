1381b1579fdae77efabc5b60fcf5378e8d467463
Kaylie Hollander <Kphollander1@gmail.com>
2019-01-15 08:56:39 -0700

    Initial commit

---
+++ CollectionViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  CollectionViewCell.swift
+//  MegaCSP
+//
+//  Created by Hollander, Kaylie on 1/4/19.
+//  Copyright © 2019 Hollander, Kaylie. All rights reserved.
+//
+
+import UIKit
+
+class CollectionViewCell: UICollectionViewCell {
+    
+}
